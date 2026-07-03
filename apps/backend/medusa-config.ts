fea0862e5ebc30d035bd8a49340b720fda1bce60
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2026-07-03 10:18:19 -0300

    feature/MDB-002: add redis config with the environment variable

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from '@medusajs/framework/utils';
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -11,6 +11,7 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
-    }
-  }
-})
+    },
+    redisUrl: process.env.REDIS_URL,
+  },
+});
